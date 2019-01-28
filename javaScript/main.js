1a59a94a8b1e86cf56dabece416196cbbed31219
Abdullah <ab4ullahkh@gmail.com>
2019-01-28 11:44:02 +0300

    update #3 1- created restartTheGame function for restar the game. 2- add img folder. 3- improve the style little bit.

--- javaScript/main.js
+++ javaScript/main.js
@@ -9,6 +9,8 @@
 
 let currentPlayer = "X";
 
+let ScoreX = 0;
+
 
 $('td').on('click', Play)  
 
@@ -18,8 +20,13 @@ function Play(event){
   if($(CurrentCell).text() !== 'X' && $(CurrentCell).text() !== 'O' ){
       
       $(CurrentCell).text(currentPlayer);
-
-      console.log($(CurrentCell).text());
+      if($(CurrentCell).text() === 'X'){
+      $(CurrentCell).css("background-color","rgb(114,142,255)");
+      }
+      else{
+        $(CurrentCell).css("background-color","rgb(159,142,255)"); 
+      }
+      //console.log($(CurrentCell).text());
       ChekeTheWinner();
 
       ChangeThePlayer ();
@@ -31,7 +38,7 @@ function Play(event){
     alert("Invalid field")
   }
    
-  //ChekeTheWinner()
+
 
   
 
@@ -52,73 +59,93 @@ function ChekeTheWinner(){
     // if for X
     if(one==="X" && two==="X" && Three==="X" ){
         alert("X is win")
+        restartTheGame();
+
+     
     }
 
 
     if(four==="X" && five==="X" && six==="X" ){
         alert("X is win")
+        restartTheGame();
+        ScoreX ++;
+        $('p').text("The X sore " + ScoreX);
     }
     
 
     if(seven==="X" && eight==="X" && nine==="X" ){
         alert("X is win")
+        restartTheGame();
     }
 
 
     if(one==="X" && four==="X" && seven==="X" ){
         alert("X is win")
+        restartTheGame();
     }
 
     if(two==="X" && five==="X" && eight==="X" ){
         alert("X is win")
+        restartTheGame();
     }
 
     if(Three==="X" && six==="X" && nine==="X" ){
         alert("X is win")
+        restartTheGame();
     }
 
     if(Three==="X" && five==="X" && seven==="X" ){
         alert("X is win")
+        restartTheGame();
     }
 
     if(nine==="X" && five==="X" && one==="X" ){
         alert("X is win")
+        restartTheGame();
     }
     
     // if for O
     if(one==="O" && two==="O" && Three==="O" ){
         alert("O is win")
+        restartTheGame();
     }
 
 
     if(four==="O" && five==="O" && six==="O" ){
         alert("O is win")
+        restartTheGame();
     }
     
 
     if(seven==="O" && eight==="O" && nine==="O" ){
         alert("O is win")
+        restartTheGame();
     }
 
 
     if(one==="O" && four==="O" && seven==="O" ){
         alert("O is win")
+        restartTheGame();
     }
 
     if(two==="O" && five==="O" && eight==="O" ){
         alert("O is win")
+        restartTheGame();
     }
 
     if(Three==="O" && six==="O" && nine==="O" ){
         alert("O is win")
+        restartTheGame();
     }
 
     if(Three==="O" && five==="O" && seven==="O" ){
         alert("O is win")
+        restartTheGame();
     }
 
     if(nine==="O" && five==="O" && one==="O" ){
         alert("O is win")
+        restartTheGame();
     }
 
     }
@@ -134,10 +161,19 @@ function ChekeTheWinner(){
      // change turn
      if (currentPlayer === 'X'){
         currentPlayer = 'O'
+        
     } else {
         currentPlayer = 'X'
     }
    }
+
+   function restartTheGame() {
+    $('td').empty();
+    $('td').css("background-color", "");
+    currentPlayer = "X";
+  }
+  
+
 
 
 
